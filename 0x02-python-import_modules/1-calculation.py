91fefece6c0aa35ccb16c744f63ee9a051fa50ac
Armelle <armelledinde@yahoo.fr>
2022-06-03 16:07:50 +0200

    works1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import calculator_1 as c
-a = 10
-b = 5
-i = c.add(a, b)
-m = c.sub(a, b)
-n = c.mul(a, b)
-o = c.div(a, b)
-print("{} + {} = {}" .format(a, b, i))
-print("{} + {} = {}" .format(a, b, m))
-print("{} + {} = {}" .format(a, b, n))
-print("{} + {} = {}" .format(a, b, o))
 if __name__ == "__main__":
+    import calculator_1 as c
+    a = 10
+    b = 5
+    i = c.add(a, b)
+    m = c.sub(a, b)
+    n = c.mul(a, b)
+    o = c.div(a, b)
+    print("{} + {} = {}" .format(a, b, i))
+    print("{} + {} = {}" .format(a, b, m))
+    print("{} + {} = {}" .format(a, b, n))
+    print("{} + {} = {}" .format(a, b, o))
