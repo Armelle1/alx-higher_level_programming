a3d1315c214e019f479cacc197f2365f62ce66e9
Armelle <armelledinde@yahoo.fr>
2022-06-03 11:57:07 +0200

    works2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-if n == 0:
-    print(n, "arguments.")
-if n == 1:
-    print("{} argument:" .format(n))
-    print("{}: {}" .format(n, sys.argv[n]))
-if n > 1:
-    print("{} arguments:" .format(n))
-    for i in range(n):
-        print("{}: {}" .format((i + 1), sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print(n, "arguments.")
+    if n == 1:
+        print("{} argument:" .format(n))
+        print("{}: {}" .format(n, sys.argv[n]))
+    if n > 1:
+        print("{} arguments:" .format(n))
+        for i in range(n):
+            print("{}: {}" .format((i + 1), sys.argv[i + 1]))
