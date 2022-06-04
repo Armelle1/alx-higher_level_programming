dd6c502ea5c728323eda44821db77c95a12b0b8c
Armelle <armelledinde@yahoo.fr>
2022-06-04 09:37:00 +0200

    works0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     n = len(my_list)
-    for i in range(n):
-        print("{}" .format(my_list[i]))
+    p = all ([isinstance (item, int,) for item in my_list])
+    if p == True:
+        for i in range(n):
+            print("{}" .format(my_list[i]))
