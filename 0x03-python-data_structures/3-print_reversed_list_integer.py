eb343646107e6f553e56074cc0f15d1ed75a282d
Armelle <armelledinde@yahoo.fr>
2022-06-04 17:42:27 +0200

    works3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list)
-    q = my_list[:: -1]
-    for i in range(n):
-        print("{}" .format(q[i]))
+    p = all ([isinstance (item, int,) for item in my_list])
+    if (p == True):
+         q = my_list[:: -1]
+         for i in range(n):
+             print("{}" .format(q[i]))
