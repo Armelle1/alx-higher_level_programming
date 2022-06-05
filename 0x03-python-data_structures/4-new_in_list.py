399204fa109d44f3df01a6ed38b411686a653c96
Armelle <armelledinde@yahoo.fr>
2022-06-05 19:43:57 +0200

    works4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 def new_in_list(my_list, idx, element):
     n = len(my_list)
     p = tuple(my_list)
+    m = list(p)
     if idx < 0:
         return my_list
     if idx > n - 1:
         return my_list
     if idx > 0:
-        my_list[idx] = element
-        m = list(p)
-        return m
+        m[idx] = element
+    return m
