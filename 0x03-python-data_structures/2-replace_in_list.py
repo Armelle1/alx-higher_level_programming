45f665de83e199021f00ec197872479081c57430
Armelle <armelledinde@yahoo.fr>
2022-06-05 20:37:37 +0200

    works2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx > n - 1:
         return my_list
-    if idx > 0:
+    if idx >= 0:
         my_list[idx] = element
         return my_list
