f2d6915bdc0acc734a1587a3ed47961b9008f5a2
Armelle <armelledinde@yahoo.fr>
2022-06-06 23:09:23 +0200

    work 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     # for i in matrix:
     #    m = ' '.join(map(str, i))
     for row in matrix:
-        for item in row:
-            print("{:d} " .format(item), end=" ")
-        print()
+        size = len(row)
+        for i in range(size):
+            if i < size - 1:
+                print("{:d} " .format(row[i]), end=" ")
+            else:
+                print("{:d}" .format(row[i]))
