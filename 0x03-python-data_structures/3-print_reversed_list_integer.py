5eefa84bad4e3d64cd44ce7c9a2689121f97acd6
Armelle <armelledinde@yahoo.fr>
2022-06-06 22:03:02 +0200

    work 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list)
+    if n <= 0:
+        return None
     q = my_list[:: -1]
     for i in range(n):
         print("{:d}".format(q[i]))
