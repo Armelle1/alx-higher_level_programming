3e6d099686b447362f7bcfa4775925c58f60df7d
Armelle <armelledinde@yahoo.fr>
2022-06-08 13:07:03 +0200

    works6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     p = a_dictionary.keys()
-    print(p)
     q = sorted(p)
     for i in q:
-        print(i, ":", a_dictionary[i])
+        s = print(i, ":", a_dictionary[i])
+    return s
