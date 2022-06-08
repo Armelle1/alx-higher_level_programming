f3f525d302e5308663bc47d8e4e81dc3bdedff15
Armelle <armelledinde@yahoo.fr>
2022-06-08 22:20:56 +0200

    work 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     first_key = list(a_dictionary)[0]
-    i = 0
+    i = -1000
     for p in a_dictionary:
         if a_dictionary[p] > i:
             i = a_dictionary[p]
