5b84c84f8a0f84783b4d98abbde89fdc06f8a8b0
Armelle <armelledinde@yahoo.fr>
2022-06-20 13:12:38 +0200

    works0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        p = my_list[0:x]
+        for i in p:
+            print(i, end='')
+            c = c + 1
+    except:
+        pass
+    print()
+    return c
