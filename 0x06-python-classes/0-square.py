bf38775a13fb711b87f1a0dd1f7d734b3d3a16a2
Armelle <armelledinde@yahoo.fr>
2022-06-21 20:53:35 +0200

    works0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    '''
+    The class square contains info about all the accounts
+
+    Args:
+        arg (str): The arg is used for...
+        *args: The variable arguments are used for...
+        **kwargs: The keyword arguments are used for...
+
+    Attributes:
+        arg (str): This is where we store arg,
+    '''
     pass
