3d3468710d02f6a025f0763bf1da74a16459712d
Armelle <armelledinde@yahoo.fr>
2022-06-27 17:33:44 +0200

    works2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+''' this method defines a rectangle'''
+
+
+class Rectangle:
+    ''' this class defines a rectangle '''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        '''this methode retrieve the width of rectangles'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        ''' this methode set the value of width '''
+        if not type(value) is int:
+            raise TypeError('width must be an integer')
+        if (value < 0):
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        ''' this methode gets the height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' this methode set the value of height'''
+        if not type(value) is int:
+            raise TypeError('height must be an integer')
+        if (value < 0):
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+
+        ''' return the rectangle area'''
+        c = self.__width * self.__height
+        return c
+
+    def perimeter(self):
+        ''' return the rectangle perimeter '''
+        c = (self.__width + self. __height) * 2
+        if (self.__width == 0 or self.__height == 0):
+            c = 0
+        return c
