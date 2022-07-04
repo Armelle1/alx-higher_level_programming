35afb14ac0c133f0fd31d675be59fee2dda2380d
Armelle <armelledinde@yahoo.fr>
2022-07-04 17:19:13 +0200

    works2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 ''' this module returns the instance of an object'''
+
+
 def is_same_class(obj, a_class):
+    ''' this class return True for the sepcified class'''
     if type(obj) is a_class:
         return True
     else:
