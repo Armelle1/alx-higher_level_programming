1ddd8fd61beca46a91b2229ec70902e974100749
Armelle <armelledinde@yahoo.fr>
2022-07-04 17:40:24 +0200

    works4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' this module'''
+
+
+def inherits_from(obj, a_class):
+    ''' this methode'''
+    if not (type(obj) == a_class):
+        return True
+    else:
+        return False
