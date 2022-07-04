e38353a7c96c2ff096101a112fbdb8ef7e113eeb
Armelle <armelledinde@yahoo.fr>
2022-07-04 19:29:45 +0200

    works6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' this module geometry module '''
+
+
+class BaseGeometry:
+    ''' this class '''
+    def area(self):
+        ''' this methode '''
+        raise Exception('area() is not implemented')
