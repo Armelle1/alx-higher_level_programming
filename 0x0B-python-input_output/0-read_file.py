9cefdec2dfd113fe6af3dd398166132bb9586a37
Armelle <armelledinde@yahoo.fr>
2022-07-05 15:43:37 +0200

    works0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' this method'''
+
+
+def read_file(filename=""):
+    ''' this module '''
+    with open('my_file_0.txt', encoding="utf-8") as f:
+        read_data = f.read()
